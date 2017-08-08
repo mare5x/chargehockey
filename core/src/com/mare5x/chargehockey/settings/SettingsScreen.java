b4e5f1b0505716a5b8f4ebcc47d4be227ef4eec3
mare5x <mare5x.mare5x@gmail.com>
2017-08-08 14:08:00 +0200

    Added a new utility SettingSlider to the SettingsScreen

--- core/src/com/mare5x/chargehockey/settings/SettingsScreen.java
+++ core/src/com/mare5x/chargehockey/settings/SettingsScreen.java
@@ -27,7 +27,7 @@ public class SettingsScreen extends BaseMenuScreen {
     private final SettingsFile settings_file;
 
     private final SettingCheckBox acceleration_checkbox, velocity_checkbox, trace_path_checkbox, forces_checkbox;
-    private final Slider game_speed_slider;
+    private final SettingSlider game_speed_slider;
 
     public SettingsScreen(final ChargeHockeyGame game, final Screen parent_screen) {
         super(game);
@@ -37,32 +37,18 @@ public class SettingsScreen extends BaseMenuScreen {
 
         settings_file = new SettingsFile();
 
-        final Label game_speed_label = new Label("GAME SPEED: ", game.skin);
-        game_speed_label.setWrap(true);
-        game_speed_label.setAlignment(Align.center);
-        game_speed_slider = new Slider(0.1f, 1.5f, 0.1f, false, game.skin);
-        game_speed_slider.setValue(settings_file.getFloat(SETTINGS_KEY.GAME_SPEED));
-        game_speed_label.setText(String.format(Locale.US, "GAME SPEED: %.1f", game_speed_slider.getValue()));
-        game_speed_slider.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                game_speed_label.setText(String.format(Locale.US, "GAME SPEED: %.1f", game_speed_slider.getValue()));
-            }
-        });
+        game_speed_slider = new SettingSlider(game, 0.1f, 1.5f, settings_file.getFloat(SETTINGS_KEY.GAME_SPEED));
+        game_speed_slider.set_label_format("GAME SPEED: %.1f");
 
-        velocity_checkbox = new SettingCheckBox(game, "SHOW VELOCITY VECTOR", SETTINGS_KEY.SHOW_VELOCITY_VECTOR);
-        acceleration_checkbox = new SettingCheckBox(game, "SHOW ACCELERATION VECTOR", SETTINGS_KEY.SHOW_ACCELERATION_VECTOR);
-        forces_checkbox = new SettingCheckBox(game, "SHOW FORCE VECTORS", SETTINGS_KEY.SHOW_FORCE_VECTORS);
-        trace_path_checkbox = new SettingCheckBox(game, "TRACE PATH?", SETTINGS_KEY.TRACE_PATH);
+        velocity_checkbox = new SettingCheckBox(game, "SHOW VELOCITY VECTOR", settings_file.getBoolean(SETTINGS_KEY.SHOW_VELOCITY_VECTOR));
+        acceleration_checkbox = new SettingCheckBox(game, "SHOW ACCELERATION VECTOR", settings_file.getBoolean(SETTINGS_KEY.SHOW_ACCELERATION_VECTOR));
+        forces_checkbox = new SettingCheckBox(game, "SHOW FORCE VECTORS", settings_file.getBoolean(SETTINGS_KEY.SHOW_FORCE_VECTORS));
+        trace_path_checkbox = new SettingCheckBox(game, "TRACE PATH?", settings_file.getBoolean(SETTINGS_KEY.TRACE_PATH));
 
         add_back_button(game.skin);
         table.add().expand().colspan(2).row();
 
-        Table slider_table = new Table();
-        slider_table.add(game_speed_label).pad(15).width(Value.percentWidth(0.4f, table)).fill().center();
-        slider_table.add(game_speed_slider).pad(15).width(Value.percentWidth(0.4f, table)).expandX().fillX().row();
-        table.add(slider_table).colspan(2).pad(15).row();
-
+        game_speed_slider.add_to_table(table);
         velocity_checkbox.add_to_table(table);
         acceleration_checkbox.add_to_table(table);
         forces_checkbox.add_to_table(table);
@@ -78,7 +64,7 @@ public class SettingsScreen extends BaseMenuScreen {
 
     private void save() {
         Gdx.app.log("SettingsScreen", "saving preferences");
-        settings_file.put(SETTINGS_KEY.GAME_SPEED, game_speed_slider.getValue());
+        settings_file.put(SETTINGS_KEY.GAME_SPEED, game_speed_slider.get_value());
         settings_file.put(SETTINGS_KEY.SHOW_VELOCITY_VECTOR, velocity_checkbox.is_checked());
         settings_file.put(SETTINGS_KEY.SHOW_ACCELERATION_VECTOR, acceleration_checkbox.is_checked());
         settings_file.put(SETTINGS_KEY.SHOW_FORCE_VECTORS, forces_checkbox.is_checked());
@@ -90,9 +76,7 @@ public class SettingsScreen extends BaseMenuScreen {
     public void resize(int width, int height) {
         super.resize(width, height);
 
-        // work around knob size
-        game_speed_slider.getStyle().knob.setMinHeight(width * 0.125f);
-        game_speed_slider.getStyle().knob.setMinWidth(width * 0.125f);
+        game_speed_slider.set_knob_size(width * 0.125f);
 
         Gdx.graphics.requestRendering();
     }
@@ -112,13 +96,13 @@ public class SettingsScreen extends BaseMenuScreen {
     }
 
     /** Encapsulates a unified checkbox. Use with add_to_table(). DO NOT use it as an Actor. */
-    private class SettingCheckBox extends Actor {
+    private static class SettingCheckBox extends Actor {
         private final Button checkbox;
         private final TextButton text_button;
 
-        SettingCheckBox(final ChargeHockeyGame game, String label, SETTINGS_KEY setting) {
+        SettingCheckBox(final ChargeHockeyGame game, String label, boolean checked) {
             checkbox = new Button(game.skin, "checkbox");
-            checkbox.setChecked(settings_file.getBoolean(setting));
+            checkbox.setChecked(checked);
             checkbox.pad(10);
 
             text_button = new TextButton(label, game.skin);
@@ -150,6 +134,53 @@ public class SettingsScreen extends BaseMenuScreen {
         @Override
         public float getWidth() {
             return text_button.getWidth();
+        }
+    }
+
+    /** A unified horizontal slider. Do NOT use it as a Table, but use the add_to_table method. */
+    private static class SettingSlider extends Table {
+        private final Label label;
+        private final Slider slider;
+
+        SettingSlider(final ChargeHockeyGame game, float min, float max, float current) {
+            label = new Label("...", game.skin);
+            label.setWrap(true);
+            label.setAlignment(Align.center);
+
+            slider = new Slider(min, max, 0.1f, false, game.skin);
+            slider.setValue(current);
+        }
+
+        void add_to_table(Table parent) {
+            add(label).pad(15).width(Value.percentWidth(0.4f, parent)).fill().center();
+            add(slider).pad(15).width(Value.percentWidth(0.4f, parent)).expandX().fillX().row();
+            parent.add(this).colspan(2).row();
+        }
+
+        void set_text(String text) {
+            label.setText(text);
+        }
+
+        float get_value() {
+            return slider.getValue();
+        }
+
+        void set_knob_size(float size) {
+            // work around knob size
+            slider.getStyle().knob.setMinHeight(size);
+            slider.getStyle().knob.setMinWidth(size);
+        }
+
+        /** Registers the built in label to display the formatted format string using the
+         * slider's current value. i.e. format must have a single '%f' or '%d' tag. */
+        void set_label_format(final String format) {
+            set_text(String.format(Locale.US, format, get_value()));
+            slider.addListener(new ChangeListener() {
+                @Override
+                public void changed(ChangeEvent event, Actor actor) {
+                    set_text(String.format(Locale.US, format, get_value()));
+                }
+            });
         }
     }
 }
